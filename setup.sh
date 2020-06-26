14fb8da74deedc5f4591a22cbcd6875bd8d3c07e
Dante <35383529+srdante@users.noreply.github.com>
2020-06-26 15:07:46 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,18 +8,14 @@ sudo wo stack install --ufw
 sudo wo stack install --fail2ban
 sudo wo stack install --sendmail
 
-# Config
-sudo ufw allow 22,80,443,22222/tcp
-sudo ufw enable
-
 # Supervisor
 sudo apt-get install supervisor -y
 sudo service supervisor restart
 
-# Cron
-sudo apt-get install cron -y
-sudo systemctl enable cron
-
 # Dropbox
 sudo mkdir /root/dropbox/
 sudo curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o /root/dropbox/dropbox_uploader.sh
+
+# Config
+sudo ufw allow 22,80,443,22222/tcp
+sudo ufw enable
