7a13231cb17bc82212eeef034323a85c182ee946
Dante <35383529+srdante@users.noreply.github.com>
2020-06-26 16:36:36 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,9 +17,6 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y gcc g++ make
 
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
-
 # Supervisor
 sudo apt-get install supervisor -y
 sudo service supervisor restart
