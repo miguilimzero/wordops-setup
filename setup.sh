ad40a706c092da65f86a3cb4a04cef85e2b9953d
Dante <35383529+srdante@users.noreply.github.com>
2020-06-26 08:33:30 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ sudo wo stack install --ufw
 sudo wo stack install --fail2ban
 sudo wo stack install --sendmail
 
-sudo apt-get install supervisor
+sudo apt-get install supervisor -y
 service supervisor restart
 
-sudo apt-get install cron
+sudo apt-get install cron -y
 sudo systemctl enable cron
