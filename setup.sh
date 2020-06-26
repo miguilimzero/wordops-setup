cc5b0859bbb91a7c1490833d3d19cde3ede02618
Dante <35383529+srdante@users.noreply.github.com>
2020-06-26 11:14:03 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ sudo apt-get install cron -y
 sudo systemctl enable cron
 
 # Dropbox
-mkdir /root/dropbox/
-curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o /root/dropbox/dropbox_uploader.sh
-chmod +x /root/dropbox/dropbox_uploader.sh
-./root/dropbox/dropbox_uploader.sh
+sudo mkdir /root/dropbox/
+sudo curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o /root/dropbox/dropbox_uploader.sh
+sudo chmod +x /root/dropbox/dropbox_uploader.sh
+sudo ./root/dropbox/dropbox_uploader.sh
