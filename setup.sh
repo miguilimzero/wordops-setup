6899157986355988bc5ab34002f5a42e58813536
Dante <35383529+srdante@users.noreply.github.com>
2020-06-26 11:52:04 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,5 +23,3 @@ sudo systemctl enable cron
 # Dropbox
 sudo mkdir /root/dropbox/
 sudo curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o /root/dropbox/dropbox_uploader.sh
-#sudo chmod +x /root/dropbox/dropbox_uploader.sh
-bash /root/dropbox/dropbox_uploader.sh
